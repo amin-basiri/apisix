54a9d2441eb5ea11f2589ccb50901b59d5efbe29
Amin <amin.bsr99@gmail.com>
2023-10-15 13:11:10 +0330

    update generate_number.sh

--- generate_number.sh
+++ generate_number.sh
@@ -1,4 +1,4 @@
 hexdump -v -e '5/1 "%02x""\n"' /dev/urandom |
   awk -v OFS=',' '
-    { print 9int(NR * 32768 * rand()), " H" }' |
+    { print 9int(NR * 32419768 * rand()), " H" }' |
   head -n "$1" > numbers.csv
