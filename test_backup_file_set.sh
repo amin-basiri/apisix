150335517bb5d5e67d8210c4ee0a4d6cb7511c51
Amin <amin.bsr99@gmail.com>
2023-11-25 18:57:46 +0330

    do staff

--- test_backup_file_set.sh
+++ test_backup_file_set.sh
@@ -2,7 +2,7 @@ start=$(date +%s%N)
 
 
 # Code Here
-curl -i -X POST "http://127.0.0.1:9080/manage_redis_numbers?type=file" -H "Host: example.com" -F msisdns=@numbers.csv 
+curl -i -X POST "http://127.0.0.1:9080/manage_redis_numbers?type=file" -H "Host: example.com" -F msisdns=@numbers_2.csv 
 ###########
 
 
